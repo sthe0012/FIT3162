1ccc11a471328b7618c2cdd71e9f916d0bac0be7
tyoppar01 <145849049+tyoppar01@users.noreply.github.com>
2024-04-18 11:33:28 +1000

    Add Tabs
    add tabs based on project proposal
    home, about, deception, dataset, contact

--- user_interface/ui.py
+++ user_interface/ui.py
@@ -4,6 +4,21 @@ import gradio as gr
 with gr.Blocks() as demo:
     
     gr.Markdown(""" # MCS4 - Securing Face ID """)
+    
+    with gr.Tab("Home"):
+        gr.Markdown(""" init """)
+    
+    with gr.Tab("About"):
+        gr.Markdown(""" init """)
+    
+    with gr.Tab("Deception"):
+        gr.Markdown(""" init """)
+
+    with gr.Tab("Dataset"):
+        gr.Markdown(""" init """)
+      
+    with gr.Tab("Contact"):
+        gr.Markdown(""" init """)
 
 
 demo.launch()
