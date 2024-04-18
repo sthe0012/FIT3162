e2d6830059c8bea14e1dc7ebd493e7d1b05dbcde
tyoppar01 <145849049+tyoppar01@users.noreply.github.com>
2024-04-18 11:34:21 +1000

    Add Contact Tab Information
    add the team contact
    - bot user
    - supervisor

--- user_interface/ui.py
+++ user_interface/ui.py
@@ -18,7 +18,21 @@ with gr.Blocks() as demo:
         gr.Markdown(""" init """)
       
     with gr.Tab("Contact"):
-        gr.Markdown(""" init """)
+        gr.Markdown("# The Team")
+        
+        gr.Image("t2.jpg", width=150, height=200)
+        gr.Markdown("### Contact Information\n"
+                    "Please contact us at:\n"
+                    "- **Role:** Quality Assurance\n"
+                    "- **Email:** example@email.com\n"
+                    "- **Phone:** +123456789")
+        
+        gr.Image("t5.jpg", width=150, height=200)
+        gr.Markdown("### Contact Information\n"
+                    "Please contact us at:\n"
+                    "- **Role:** Supervisor\n"
+                    "- **Email:** leong.shumin@monash.edu\n"
+                    "- **Phone:** +603-5516 1892")
 
 
 demo.launch()
